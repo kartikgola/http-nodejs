5f9e5fa330e0413d2c85dc59e7f6a85e8e7d2432
Kartik Gola <11961841+kartikgola@users.noreply.github.com>
2026-04-23 01:38:01 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,9 @@
 import { createServer } from 'http';
+const port = process.env.PORT || 3000;
 
 createServer((req, res) => {
   res.write('Hello World!');
   res.end();
-}).listen(process.env.PORT || 3000, '::');
+}).listen(port, '::', () => {
+  console.log(`Server is definitely listening on port: ${port}`);
+});
